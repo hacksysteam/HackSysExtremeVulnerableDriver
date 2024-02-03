65748830ed08be7ed409fb5593af3e7fb1d85f9a
Divyanshu | seg_fault <135882723+0xDivyanshu-new@users.noreply.github.com>
2024-02-03 14:43:09 +0530

    Update IntegerOverflow.c

--- Driver/HEVD/Windows/IntegerOverflow.c
+++ Driver/HEVD/Windows/IntegerOverflow.c
@@ -114,7 +114,7 @@ TriggerIntegerOverflow(
         // integer will wrap down and will finally cause this check to fail
         //
 
-        if ((Size + TerminatorSize) > (ULONG)sizeof(KernelBuffer)))
+        if ((UINT32)(Size + TerminatorSize) > (UINT32)sizeof(KernelBuffer)))
         {
             DbgPrint("[-] Invalid UserBuffer Size: 0x%X\n", Size);
 
